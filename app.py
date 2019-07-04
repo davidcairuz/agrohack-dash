96e397e7bac397c599ad0e691185036438c9a9e4
David Cairuz <davidcairuz@gmail.com>
2019-07-04 13:54:48 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-wanted_columns = ['Custo total','Depreciacao','Faturamento Bruto','Receita Liquida','Lucro Bruto','Resultado']
+wanted_columns = ['Custo total','Depreciação','Faturamento Bruto','Receita Liquida','Lucro Bruto','Resultado']
+graph_columns = ['Alimentação','Mão-de-Obra','Sanidade','Reposição','Impostos','Despesas Diversas','Pró-Labore','Depreciação']
 
 df = pd.read_csv('clientes.csv')
 df.columns = df.columns.str.strip()
@@ -17,7 +18,7 @@ us = df.iloc[0]
 
 def plot_pie(df):
     us = df.iloc[0]
-    pie_labels = wanted_columns
+    pie_labels = graph_columns
     pie_values = [us[i] for i in pie_labels]
 
     pie_trace = go.Pie(labels=pie_labels, values=pie_values)
@@ -30,8 +31,9 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
-home_layout = html.Div(children=[
-    html.H1(children=df['Nome da Fazenda'][0], style={'text-align':'center'}),
+home_layout = html.Div(style={'background-color':'#fafcff'}, children=[
+    html.Link(href='https://fonts.googleapis.com/css?family=Oxygen&display=swap'),
+    html.H1(children=df['Nome da Fazenda'][0], style={'text-align':'center', 'font-family': "'Oxygen', sans-serif", 'padding-top': '30px'}),
 
     # Grid Layout
     html.Div(children=[
@@ -39,7 +41,7 @@ home_layout = html.Div(children=[
         # Information block
         html.Div(children=[
 
-            html.H3('Panorama Geral'),
+            html.H3('Panorama geral', style={'font-family': "'Oxygen', sans-serif"}),
 
             # Information grid container (preenchido pela função update_value)
             html.Div(className='info-grid-container', id='info-grid-container'),
@@ -47,14 +49,14 @@ home_layout = html.Div(children=[
             # Atualizar informações
             html.Div(children=[
                 html.Br(),
-                html.H3('Atualizar informações'),
+                html.H3('Atualizar informações',  style={'font-family': "'Oxygen', sans-serif"}),
 
                 # Dropdown para seleção de atributo
                 html.Span('Selecione um indicador'),
                 html.Div(children=[
                     dcc.Dropdown(
                         id='atr-change',
-                        options=[{'label': i, 'value': i} for i in wanted_columns],
+                        options=[{'label': i, 'value': i} for i in graph_columns],
                         value='Custo total'
                     )
                 ], className='dropdown-update'),
@@ -65,7 +67,7 @@ home_layout = html.Div(children=[
                 html.Span('Insira o novo valor do campo'),
                 html.Div(children=[
                     dcc.Input(id='new-value', value=0, type='number'),
-                    html.Button(id='submit-button', n_clicks=0, children='Atualizar'),
+                    html.Button(id='submit-button', n_clicks=0, children='Atualizar', className='update-button'),
                 ], className='input-newvalue', style={'display':'flex'})
 
             ], className='update-info'),
@@ -80,14 +82,21 @@ home_layout = html.Div(children=[
                     'data': [
                         plot_pie(df)
                     ],
+                    'layout': go.Layout(
+                        autosize = False,
+                        width=620,
+                        height=620,
+                        paper_bgcolor='#fafcff'
+                    )
                 }
+                
             )
         ], className='graph'),
 
         # Buttons block
         html.Div(children=[
             
-            html.H3('Menu de opções'),
+            html.H3('Menu de opções',  style={'font-family': "'Oxygen', sans-serif"}),
 
             # Buttons grid container
             html.Div(children=[
@@ -139,6 +148,156 @@ home_layout = html.Div(children=[
 # Página de detalhes da nutrição
 nutricao_layout = html.Div(children=[
     html.H1(children=df['Nome da Fazenda'][0] + ' - Nutrição', style={'text-align':'center'}),
+
+    # Grid Layout
+    html.Div(children=[
+        
+        # Simulação
+        html.Div(children=[
+            html.H3('Simulação'),
+
+            # Simulação grid
+            html.Div(children=[
+                
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Receita Liquida']), html.H4(id="receitaText"), html.P("Receita Líquida")],
+                        id="receita",
+                        className="mini-container-simulation",
+                    ),
+
+                ], className='total-relation'),
+
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Receita Liquida']), html.H4(id="receitaText"), html.P("Receita Líquida")],
+                        id="receita2",
+                        className="mini-container-simulation",
+                    )
+                ], className='rendabruta-relation'),
+
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Receita Liquida']), html.H4(id="receitaText"), html.P("Receita Líquida")],
+                        id="receita2",
+                        className="mini-container-simulation-large",
+                    ),
+
+                ], className='lucro'),
+
+                html.Div(children=[
+                    html.P('Eficiência (%)'),
+                    html.Div(children=[
+                        dcc.Slider(
+                            id='percent-slider-eficiency',
+                            min=0,
+                            max=100,
+                            value=0,
+                            marks={i: i for i in range(100, 10)},
+                            step=5
+                        ), 
+                    ])
+                ], className='slider1', style={'padding-right':'10px', 'padding-top':'10px'}),
+
+                html.Div(children=[
+                    html.P('Custo (%)'),
+                    html.Div(children=[
+                        dcc.Slider(
+                            id='percent-slider-cost',
+                            min=0,
+                            max=100,
+                            value=0,
+                            marks={i: i for i in range(100, 10)},
+                            step=5
+                        ),
+                    ])
+                ], className='slider2', style={'padding-left':'20px', 'padding-top':'10px'}),
+
+            ], className='simulation-grid-container'),
+            
+            html.Div(children=[
+                dcc.Graph(
+                    figure = {
+                        'data': [
+                            go.Bar(
+                                x = ['Lucro real/ha', 'Lucro ótimo/ha'],
+                                y = [df['Resultado'][0]/200, 988.98],
+                                name = 'Lucro'
+                            )    
+                        ],
+                    }
+                )
+            ], className='graph-bar')
+
+        ], className='simulation'),
+
+        # Informações detalhadas
+        html.Div(children=[
+            html.H3('Informações detalhadas'),
+            html.Div(children=[
+                # Information 1
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Custo total']), html.H4(id="totalText"), html.P("Custo total")],
+                        id="total",
+                        className="mini-container-detailed",
+                    )
+                ], className='mini-container1'),
+                
+                # Information 2
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Depreciação']), html.H4(id="depreText"), html.P("Depreciação")],
+                        id="depre",
+                        className="mini-container-detailed",
+                    )
+                ], className='mini-container2'),
+
+                # Information 3
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Faturamento Bruto']), html.H4(id="fatText"), html.P("Faturamento")],
+                        id="fat",
+                        className="mini-container-detailed",
+                    )
+                ], className='mini-container3'),
+                
+                # Information 4
+                html.Div(children=[
+                    html.Div(
+                        ['R$ ' + str(us['Receita Liquida']), html.H4(id="receitaText"), html.P("Receita Líquida")],
+                        id="receita",
+                        className="mini-container-detailed",
+                    )
+                ], className='mini-container4'),
+
+            ], className='detailed-info-grid')
+
+        ], className='detailed-information'),
+
+        # Startups
+        html.Div(children=[
+             html.H3("Recomendações"),
+
+             html.Div(children=[
+                 
+                html.Div(children=[
+                    dcc.Link(html.Img(src='assets/images/ylive biotecnologia.png'))
+                ], className='startup1'),
+
+                html.Div(children=[
+                    dcc.Link(html.Img(src='assets/images/bbq-removebg-preview.png'))
+                ], className='startup2'),
+                
+                html.Div(children=[
+                    dcc.Link(html.Img(src='assets/images/Algae-Biotecnologia-removebg-preview.png'))
+                ], className='startup3'),
+
+             ], className='startup-grid-container'),
+
+        ], className='startups'),
+
+    ], className='detailed-grid-container')
 
 ])
 
@@ -205,7 +364,6 @@ def display_page(pathname):
 def update_value(n_clicks, field, new_value):
     if n_clicks > 0:
         df[field][0] = new_value
-    #df.to_csv("cliente.csv", index=False)
 
     us = df.iloc[0]
 
@@ -222,7 +380,7 @@ def update_value(n_clicks, field, new_value):
         # Information 2
         html.Div(children=[
             html.Div(
-                ['R$ ' + str(us['Depreciacao']), html.H4(id="depreText"), html.P("Depreciação")],
+                ['R$ ' + str(us['Depreciação']), html.H4(id="depreText"), html.P("Depreciação")],
                 id="depre",
                 className="mini-container",
             )
@@ -258,14 +416,19 @@ def update_value(n_clicks, field, new_value):
         # Information 6
         html.Div(children=[
             html.Div(
-                ['R$ ' + str(us['Resultado']), html.H4(id="resultText"), html.P("Resultado")],
+                ['R$ ' + str(us['Resultado']), html.H4(id="resultText"), html.P("Resultado da op.")],
                 id="result",
                 className="mini-container",
             )
         ], className='info6'),
     ], {'data': [
         plot_pie(df)
-    ]}
+    ], 'layout': go.Layout(
+                        autosize = False,
+                        width=620,
+                        height=620,
+                        paper_bgcolor='#fafcff'
+                    )}
 
 if __name__ == '__main__':
     app.run_server(debug=True)
